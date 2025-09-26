0ce2ca1c3e629f093313253288cef4602c9de302
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-09-26 12:08:00 -0500

    feat ( src/hello-world/hello-world.resolver.ts ) Creating my first operations in GraphQL

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,11 +1,38 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query( () => String)
+    @Query( () => String, {
+        description: 'Hola mundo es lo que retorna.',
+        name: 'hello'
+    })
     helloWorld(): string {
         return 'Hola Mundo'
     }
+
+    @Query( () => Number, {
+        name: 'randomNumber',
+    })
+    getRandomNumber(): number {
+        return Math.random() * 100;
+    }
+
+    @Query( () => Int, {
+        name: 'randomFromZeroTo',
+        description: 'Get number from zero to argument TO (default 6)'
+    })
+    getRandomFromZeroTo( 
+        @Args('to',{
+            type: () => Int,
+            defaultValue: 6,
+            nullable: true
+        }) 
+        to: number ): number{
+
+        return Math.round( Math.random() * to );
+    }
+
+
 
 }
