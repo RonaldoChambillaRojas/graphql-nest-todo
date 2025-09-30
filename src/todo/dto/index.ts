d19f2a3c3113e71af32caf66a397fd72abbfd848
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-09-29 21:44:43 -0500

    chore ( src/todo/dto/index.ts ) Creating a barrel file to handle Args and Inputs exports

---
+++ src/todo/dto/index.ts
@@ -0,0 +1,4 @@
+export { UpdateTodoInput } from "./inputs/update-todo.input";
+export { CreateTodoInput } from "./inputs/create-todo.input";
+export { StatusTodoArgs } from "./args/status-todo.args";
+
